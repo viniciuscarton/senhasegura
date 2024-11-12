32eeb9a3d613c795a3c27ad521f0fe2e16bcddc9
Vinicius <viniciuscarton@outlook>
2024-11-12 01:28:41 -0300

    Updates

---
+++ pt/assets/js/acecf23e.f967a054.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunksenhasegura=self.webpackChunksenhasegura||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/pt/blog","blogTitle":"Blog","authorsListPath":"/pt/blog/authors"}')}}]);
